4a46bb4ecae884e035d5ffbccf02b4e7ae8c7668
Alex <NaudisGarcia51@gmail.com>
2023-02-18 17:56:47 -0400

    0.01 servidor y conexión a la Base de Datos

---
+++ index.js
@@ -0,0 +1,27 @@
+// Importar dependencias
+const connection = require('./src/database/connection')
+const express = require('express')
+const cors = require('cors')
+
+// conexión a la Base de Datos
+connection()
+
+//Crear servidor Node
+const app = express()
+const puerto = 3900
+
+// Configurar Cors
+app.use(cors())
+
+// Convertir los datos del body a json
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Cargar conf rutas
+
+
+
+// Poner servidor a escuchar peticiones http
+app.listen(puerto, () => {
+    console.log('Servidor de node corriendo en el puerto  3900')
+})
